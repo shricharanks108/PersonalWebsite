1b4c0b711b6530fed542434bdad0585870192e8c
Shricharan K.S <62522322+shricharanks108@users.noreply.github.com>
2022-09-13 01:04:19 -0400

    Fix Page Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Main from "./sections/Main";
 import { ThemeProvider } from "styled-components";
 import { themes } from "./theme";
 import { GlobalStyles } from "./global";
 import { settings } from "./portfolio";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import Home from "./pages/home/HomeComponent";
+import Splash from "./pages/splash/Splash";
+import Education from "./pages/education/EducationComponent";
+import Experience from "./pages/experience/Experience";
+import Contact from "./pages/contact/ContactComponent";
+import Projects from "./pages/projects/Projects";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
@@ -14,7 +20,73 @@ function App() {
       <>
         <GlobalStyles />
         <div>
-          <Main theme={themes[theme]} setTheme={setTheme} />
+          <HashRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Splash
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+              <Route
+                path="home"
+                element={
+                  <Home
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+              <Route
+                path="experience"
+                element={
+                  <Experience
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+              <Route
+                path="education"
+                element={
+                  <Education
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+              <Route
+                path="contact"
+                element={
+                  <Contact
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+              <Route
+                path="splash"
+                element={
+                  <Splash
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+              <Route
+                path="projects"
+                element={
+                  <Projects
+                    theme={themes[theme]}
+                    setTheme={setTheme}
+                  />
+                }
+              />
+            </Routes>
+          </HashRouter>
         </div>
       </>
     </ThemeProvider>
