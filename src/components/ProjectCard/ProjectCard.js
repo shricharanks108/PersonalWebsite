68a05a11a83e0e58b30e6877ad92bd62b06e1e99
Shricharan K.S <62522322+shricharanks108@users.noreply.github.com>
2022-09-13 01:03:41 -0400

    Add Modal Feature

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,12 +3,48 @@ import ProjectLanguages from "../projectLanguages/ProjectLanguages";
 import "./ProjectCard.css";
 import { Fade } from "react-reveal";
 import { style } from "glamor";
+import { Button, Modal } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function MyVerticallyCenteredModal(props) {
+  function openRepoinNewTab(url) {
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.repo.name}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>{props.repo.name}</h4>
+        <p>
+          {props.repo.description}
+        </p>
+      </Modal.Body>
+      <Modal.Footer style={{ justifyContent: "center" }}>
+
+        <Button onClick={() => openRepoinNewTab(props.repo.url)} style={{ width: "75%" }}>Open in GitHub</Button>
+
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export default function ProjectCard({ repo, theme }) {
   function openRepoinNewTab(url) {
     var win = window.open(url, "_blank");
     win.focus();
   }
+
+  const [modalShow, setModalShow] = React.useState(false);
 
   const styles = style({
     color: "rgb(88, 96, 105)",
@@ -30,20 +66,34 @@ export default function ProjectCard({ repo, theme }) {
         <div
           {...styles}
           key={repo.id}
-          onClick={() => openRepoinNewTab(repo.url)}
-          style={{ backgroundColor: theme.projectCard }}
+          // onClick={() => openRepoinNewTab(repo.url)}
+          style={{ backgroundColor: theme.projectCard, display: "flex", flexDirection: "column", justifyContent: "space-between" }}
         >
-          <div className="repo-name-div">
-            <p className="repo-name" style={{ color: theme.text }}>
-              {repo.name}
+          <div>
+            <div className="repo-name-div">
+              <p className="repo-name" style={{ color: theme.text }}>
+                {repo.name}
+              </p>
+            </div>
+            <p className="repo-description" style={{ color: theme.text }} >
+              {repo.description}
             </p>
+            <div className="repo-details">
+              <ProjectLanguages logos={repo.languages} />
+            </div>
           </div>
-          <p className="repo-description" style={{ color: theme.text }} >
-            {repo.description}
-          </p>
-          <div className="repo-details">
-            <ProjectLanguages logos={repo.languages} />
+          <div>
+            <Button className="launch-modal" variant="outline-danger" style={{ width: "75%" }} onClick={() => setModalShow(true)}>
+              More Details...
+            </Button>
           </div>
+          <MyVerticallyCenteredModal
+            repo={repo}
+            theme={theme}
+            show={modalShow}
+            enforceFocus={true}
+            onHide={() => setModalShow(false)}
+          />
         </div>
       </Fade>
     </div>
